07a573f469f64a73a67ddc8efaab7c8272a82a82
Edward Iga Kigongo <igakigongo@gmail.com>
2021-01-27 04:55:30 +0100

    refactor - introduce one conditional (if) as required

--- guessinggame.sh
+++ guessinggame.sh
@@ -9,7 +9,7 @@ files_count=$(get_files_count)
 
 while read guess
 do
-    [[ ! $(echo $guess | egrep -o "^-?[0-9]+$") ]] && echo -n "not a number, try again: " && continue
+    if [[ ! $(echo $guess | egrep -o "^-?[0-9]+$") ]]; then echo -n "not a number, try again: " && continue; fi
     [[ $guess -eq $files_count ]] && echo "correct" && break
     [[ $guess -lt $files_count ]] && echo -n "too low, try again: " || echo -n "too high, try again: "
 done
