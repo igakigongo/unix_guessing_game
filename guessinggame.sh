f58d693c48f810edb41a6f44704d1038ee19966f
Edward Iga Kigongo <igakigongo@gmail.com>
2021-01-27 05:00:01 +0100

    add comments and expand if to match exercise requirement (more than 20 lines)

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+# get total number of files in current directory
+# does not include hidden files
+# does not traverse sub_directories
 function get_files_count {
     echo $(ls -l | egrep "^-" | wc -l)
 }
 
 echo -n "Guess the number of files in the current directory: "
+
 files_count=$(get_files_count)
 
 while read guess
 do
-    if [[ ! $(echo $guess | egrep -o "^-?[0-9]+$") ]]; then echo -n "not a number, try again: " && continue; fi
+    if [[ ! $(echo $guess | egrep -o "^-?[0-9]+$") ]]
+    then
+        echo -n "not a number, try again: " && continue
+    fi
     [[ $guess -eq $files_count ]] && echo "correct" && break
     [[ $guess -lt $files_count ]] && echo -n "too low, try again: " || echo -n "too high, try again: "
 done
